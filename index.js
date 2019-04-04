bb54832c1cfd7ef012fc5180f276b9c3f55420c2
Anh Duc <anhduc.hoang@vmodev.com>
2019-04-04 18:06:06 +0700

    content history

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const videoApi = require('./api/video');
 const configApi = require('./api/config');
 const deviceApi = require('./api/devices');
 const playListApi = require('./api/playlist');
+const contentHistoryApi = require('./api/content-history');
 const fileSocket = require('./api/socket/filemanager.socket');
-const path = require('path');
 
 module.exports = cms => {
   cms.app.use('/digital/video', videoApi);
   cms.app.use('/digital/config', configApi);
   cms.app.use('/digital/devices', deviceApi);
   cms.app.use('/digital/playlist', playListApi);
+  cms.app.use('/digital/content-history', contentHistoryApi);
   cms.app.use('/video', cms.express.static(cms.storage));
   cms.use(fileSocket);
 };

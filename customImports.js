fbec3b1b8cc673cd919d4280727ed0347aa54dcb
ThinhVu <vutrongthinhk7@gmail.com>
2020-03-05 17:51:09 +0700

    Import necessary files

---
+++ customImports.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import './fullcalendar/jquery'
+import './fullcalendar/moment'
+import './fullcalendar/fullcalendar'
+import './fullcalendar/fullcalendar.min.css'
+
+Vue.use(Vuetify);

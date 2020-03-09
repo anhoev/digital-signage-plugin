b698eace17c28169650605eaa17682478c475339
ThinhVu <vutrongthinhk7@gmail.com>
2020-03-09 16:46:45 +0700

    Remove full calendar

--- customImports.js
+++ customImports.js
@@ -1,7 +1,4 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
-import './fullcalendar/fullcalendar'
-import './fullcalendar/fullcalendar.min.css'
-
 Vue.use(Vuetify, {});

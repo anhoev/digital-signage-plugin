7f6608d8d96db6ec0a8fabd8db5a5b9145c8888e
ThinhVu <vutrongthinhk7@gmail.com>
2020-03-09 16:32:00 +0700

    Remove jquery & moment import. These library will be import later by fullcalendar

--- customImports.js
+++ customImports.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
-import './fullcalendar/jquery'
-import './fullcalendar/moment'
 import './fullcalendar/fullcalendar'
 import './fullcalendar/fullcalendar.min.css'
 
